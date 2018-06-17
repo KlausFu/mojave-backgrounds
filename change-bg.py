18e6ff51fcbc3e4ac4c66e258d121da7ae0a005f
Klaus Fuhrmeiter <klaus.fuhrmeister@gmx.de>
2018-06-17 13:03:55 +0200

    fixed image past 11pm and before 12:30 am

--- change-bg.py
+++ change-bg.py
@@ -5,12 +5,15 @@ from subprocess import PIPE
 time_now = datetime.now()
 time_then = datetime.now().replace(hour=0, minute=30)
 hasfound = 0
-for _ in range(17):
+for _ in range(16):
     if time_now <= time_then:
         hasfound = 1
         break
+    elif datetime.now().replace(hour=23, minute=00) < time_then < datetime.now().replace(hour=00, minute=30):
+        hasfound = 1
+        break
     else:
-        time_then = time_then + timedelta(hours=1,minutes=30)
+        time_then = time_then + timedelta(hours=1, minutes=30)
 
 time_then = time_then - timedelta(hours=1, minutes=30)
 
@@ -20,4 +23,6 @@ else:
     hours = str(time_then.hour).zfill(2)
     minutes = str(time_then.minute).zfill(2)
     print hours + ":" + minutes
-    subprocess.call(['osascript', '-e', 'tell application "System Events" to tell every desktop to set picture to "wallpapers/mojave-'+hours+'-'+minutes+'.jpeg"'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    subprocess.call(['osascript', '-e',
+                     'tell application "System Events" to tell every desktop to set picture to "wallpapers/mojave-' + hours + '-' + minutes + '.jpeg"'],
+                    stdin=PIPE, stdout=PIPE, stderr=PIPE)
