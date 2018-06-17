35ce870f75bcf1575261f8c3bd0254fd41d0c5d7
Klaus Fuhrmeiter <klaus.fuhrmeister@gmx.de>
2018-06-17 12:34:22 +0200

    initial commit

---
+++ change-bg.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+import subprocess
+from subprocess import PIPE
+
+time_now = datetime.now()
+time_then = datetime.now().replace(hour=0, minute=30)
+hasfound = 0
+for _ in range(17):
+    if time_now <= time_then:
+        hasfound = 1
+        break
+    else:
+        time_then = time_then + timedelta(hours=1,minutes=30)
+
+time_then = time_then - timedelta(hours=1, minutes=30)
+
+if hasfound == 0:
+    print "err"
+else:
+    hours = str(time_then.hour).zfill(2)
+    minutes = str(time_then.minute).zfill(2)
+    print hours + ":" + minutes
+    subprocess.call(['osascript', '-e', 'tell application "System Events" to tell every desktop to set picture to "wallpapers/mojave-'+hours+'-'+minutes+'.jpeg"'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
